ad322f8253d82902fd7cd752bf2f13839d1c886d
vashishtkumar <120776506+vashishtkumar@users.noreply.github.com>
2023-09-07 23:35:29 +0530

    Update 01Basic.js

--- 01Basic.js
+++ 01Basic.js
@@ -9,22 +9,22 @@ name1="vashisht kumar changeed the name1";
 let phoneNum;
 //console.log(phoneNum);//undefined
 
-/*JavaScript Identifiers
+
+/*:::::::::::::::::::::::::::::::::::::::::JavaScript Identifiers:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 All JavaScript variables must be identified with unique names.
-
 These unique names are called identifiers.
-
 Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
-
 The general rules for constructing names for variables (unique identifiers) are:
+1 Names can contain letters, digits, underscores, and dollar signs.
+2 Names must begin with a letter.
+3 Names can also begin with $ and _ (but we will not use it in this tutorial).
+4 Names are case sensitive (y and Y are different variables).
+5 Reserved words (like JavaScript keywords) cannot be used as names.
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
-Names can contain letters, digits, underscores, and dollar signs.
-Names must begin with a letter.
-Names can also begin with $ and _ (but we will not use it in this tutorial).
-Names are case sensitive (y and Y are different variables).
-Reserved words (like JavaScript keywords) cannot be used as names.*/
-let person = "John Doe", carName = "Volvo", price = 200;
-//console.log(person,price,carName);// John Doe volvo 200
+
+let person = "vashisht", carName = "scorpio", price = "16lakh";
+//console.log(person,price,carName);// vashisht scorpio 16lakh
 /*__________________________________________________________________________
 you can redeclare var 
 var name3;
@@ -32,48 +32,46 @@ but can't redeclare let and const
 ___________________________________________________________________________*/
 
 
-/*::::::::::::::::::::::::::::::::::::::::::::
-string concatination;
+/*::::::::::::::::::::string concatination:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 let ap="vashisht"+" "+"learning"+"javascript";
 console.log(ap);// vashisht leraning javascript
-::::::::::::::::::::::::::::::::::::::::::::*/
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
 
-/*::::::::::::::::::::::::::::::::::::::::::::
-some examles 
+/*:::::::::::::::::::Some examples of string concatination:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 let a=6+9+"8";//158
 let b="9"+8+8;//988
 console.table([a,b]);
-::::::::::::::::::::::::::::::::::::::::::::*/
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
 
-/*______________________________________________________________________________
-points to remember for let
-
+/*::::::::::::::::::Points to remember for let:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 1 Variables defined with let cannot be Redeclared but i case of var you can
-{let x = "John Doe";
+{let x = "vashisht";
 let x = 0;} not possible throw error
 
-{var x = "John Doe";
+{var x = "vashisht";
 var x = 0;} it will not throw error x will updated with value 0;
 
 2 Variables defined with let must be Declared before use
-
 3 Variables defined with let have Block Scope
+/////////////////////////////////////////////////
 {
   let x = 2;
 }
 // x can NOT be used here
-
+/////////////////////////////////////////////////////
+-------------------------------------------------------
 {
   var x = 2;
 }
 // x CAN be used here
-________________________________________________________________________________*/
+-------------------------------------------------------------
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
 
-/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-Hoisting-only can be used for var not for const and let
+/*:::::::::::::::::::::::::::::::::::Hoisting:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Hoisting-only can be used for var not for const and let
 examples
 car="scorpio";
 console.log(car)//scorpio
@@ -135,7 +133,7 @@ const car = {type:"Fiat", model:"500", color:"white"};
 // You can change a property:
 car.color = "red";
 // You can add a property:
-car.owner = "Johnson";
+car.owner = "vashisht";
 console.log(car);
 4
 const car = {type:"Fiat", model:"500", color:"white"};
